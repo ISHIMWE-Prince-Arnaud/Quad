d2b2840f305a62c64d886c1fa5954dffdcfee965
ISH k pro <princeishimwe754@gmail.com>
2025-10-15 09:45:54 +0200

    Added username autocomplete

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -61,6 +61,7 @@ const Register = () => {
               maxLength={30}
               className="w-full px-4 py-3 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-dark-bg text-gray-900 dark:text-white focus:ring-2 focus:ring-primary focus:border-transparent"
               placeholder="coolstudent123"
+              autoComplete="username"
             />
           </div>
 
