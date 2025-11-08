e175daab0cf33993ec3f848d55e05deee0175615
ISH k pro <princeishimwe754@gmail.com>
2025-11-08 21:17:38 +0200

    Add a default avatar after creating a user

--- backend/src/models/User.model.ts
+++ backend/src/models/User.model.ts
@@ -8,7 +8,13 @@ const UserSchema = new Schema<IUserDocument>(
     id: { type: String, required: true, unique: true }, // Clerk user ID
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    profileImage: { type: String },
+    profileImage: { 
+      type: String,
+      default: () => {
+        const randomNumber = Math.floor(Math.random() * 100) + 1; // 1-100
+        return `https://avatar.iran.liara.run/public/${randomNumber}`;
+      }
+    },
     bio: { type: String },
   },
   {
